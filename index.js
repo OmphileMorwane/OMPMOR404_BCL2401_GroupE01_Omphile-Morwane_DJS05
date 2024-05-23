5fb1ab57c901399a425641b2ae4fe8ee5b973b9c
Omphile Morwane <omphilemorwane@gmail.com>
2024-05-23 16:58:05 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//Initial state of the store
 const initialState = {
   count: 0,
 };
 
+//Action types
 const ADD = "ADD";
 const SUBTRACT = "SUBTRACT";
 const RESET = "RESET";
 
+//Reducer fucntions to handle actions and update state
 function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD:
@@ -19,19 +22,20 @@ function reducer(state = initialState, action) {
   }
 }
 
-//Store implementation
+//Store implementation using the reducer
 function createStore(reducer) {
   let currentState = initialState;
   let listeners = [];
-
+//Function to get current state and to dispatch actions then udate state
   return {
     getState: () => currentState,
     dispatch: (action) => {
-      currentState = reducer(currentState, action);
-      listeners.forEach((listener) => listener());
+      currentState = reducer(currentState, action);//Function to update state
+      listeners.forEach((listener) => listener()); //function to notify all listeners
     },
+    //Function to subscribe to state changes, add listener to array and then remore listener from the array
     subscribe: (listener) => {
-      listeners.push(listener);
+      listeners.push(listener); 
       return () => {
         listeners = listeners.filter(l => l !== listener);
       };
@@ -39,7 +43,7 @@ function createStore(reducer) {
   };
 }
 
-//Function that creates a store
+//Creating the store using above implementation
 const store = createStore(reducer);
 
 //Subscribe to state changes
